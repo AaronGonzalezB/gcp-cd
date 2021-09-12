714e61662741d16338229a399a991a15ac02822e
Aaron Gonzalez <aarongonzalezbe@gmail.com>
2021-09-12 04:48:49 +0000

    Modify to CD test

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Te amo cosita hermosa!'
+    return 'Te amo Estefania!'
 
 @app.route('/name/<value>')
 def name(value):
